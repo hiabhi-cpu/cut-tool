01bd1667dbbf300b2af637456ed7cf8a8cee80c1
Abhishek <kalasaraddiabhishek@gmail.com>
2025-06-14 14:17:09 +0530

    completed step2

--- main.go
+++ main.go
@@ -12,11 +12,10 @@ import (
 func main() {
 	if len(os.Args) >= 1 {
 		if strings.Contains(os.Args[len(os.Args)-1], ".csv") || strings.Contains(os.Args[len(os.Args)-1], ".tsv") { //has file name
-			fileName := os.Args[len(os.Args)-1]
-			argsCmd := os.Args[1 : len(os.Args)-1]
+
 			// fmt.Println(argsCmd)
 			// fmt.Println(fileName)
-			check(commandEntry(fileName, argsCmd))
+			check(commandEntry(os.Args))
 		} else {
 			return
 		}
@@ -24,53 +23,65 @@ func main() {
 
 }
 
-func commandEntry(fileName string, argsCmd []string) error {
+func commandEntry(argsCmd []string) error {
 	isCsv := true
 	hasD := false
+	hasF := false
+	fFlag := 0
+	dFlag := "\t"
+	fileName := ""
+	for i, rec := range argsCmd {
+		if strings.HasPrefix(rec, "-f") {
+			fieldString, err := strconv.Atoi(strings.Trim(argsCmd[i], "-f"))
+			hasF = true
+			check(err)
+			fFlag = fieldString
+		}
+		if strings.HasPrefix(rec, "-d") {
+			hasD = true
+			dFlag = strings.Trim(argsCmd[i], "-d")
+			if dFlag == "" {
+				check(errors.New("Give a delimiter for -d"))
+
+			}
+			// fmt.Println(dFlag)
+		}
+		if strings.Contains(rec, ".csv") || strings.Contains(rec, ".tsv") {
+			if strings.Contains(rec, ".csv") {
+				isCsv = true
+			} else {
+				isCsv = false
+			}
+			fileName = argsCmd[i]
+		}
+	}
+	if !hasF {
+		check(errors.New("Give an -f paramter"))
+	}
+
 	file, err := os.Open(fileName)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
-
 	reader := csv.NewReader(file)
-	if !strings.Contains(fileName, ".csv") {
-		isCsv = false
-	}
-
-	for _, rec := range argsCmd {
-		if strings.HasPrefix(rec, "-f") {
-			fieldString := strings.Trim(argsCmd[0], "-f")
-			err = commandField(reader, fieldString, isCsv, hasD)
-		}
-		if strings.HasPrefix(rec, "-d") {
-			hasD = true
-			err = commandField(reader, argsCmd[0], isCsv, hasD)
-		}
+	if isCsv {
+		commandCSV(reader, fFlag, hasD, dFlag)
+	} else {
+		commandTSV(reader, fFlag, hasD, dFlag)
 	}
 
 	if err != nil {
 		return err
 	}
+	fmt.Println(hasD)
 	return nil
 }
 
-func commandField(reader *csv.Reader, cmd string, isCsv, hasD bool) error {
-	fieldString := strings.Trim(cmd, "-f")
-	field, err := strconv.Atoi(fieldString)
-	splitStr := "\t"
-	if hasD {
-		fmt.Println(strings.Trim(cmd, "-d"))
-		splitStr = strings.Trim(cmd, "-d")
-	}
-	fmt.Println(splitStr)
-	if err != nil {
-		return err
-	}
+func commandCSV(reader *csv.Reader, field int, hasD bool, dFlag string) error {
 	if field == 0 {
 		return errors.New("Give correct column number > 0")
 	}
-	// fmt.Println(field)
 	for {
 		record, err := reader.Read()
 		if err != nil {
@@ -79,15 +90,46 @@ func commandField(reader *csv.Reader, cmd string, isCsv, hasD bool) error {
 			}
 			return err
 		}
-		// fmt.Println(record)
-		var row []string
-		row = strings.Split(record[0], splitStr)
+		if !hasD {
+			fmt.Println(record)
+			continue
+		}
+		if dFlag != "," {
+			fmt.Println(record)
+			continue
+		}
+		fmt.Println(record[field-1])
+	}
 
+	return nil
+}
+
+func commandTSV(reader *csv.Reader, field int, hasD bool, dFlag string) error {
+
+	if field == 0 {
+		return errors.New("Give correct column number > 0")
+	}
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			if strings.Contains(err.Error(), "EOF") {
+				break
+			}
+			return err
+		}
+
+		if dFlag != "\t" {
+			fmt.Println(record)
+			continue
+		}
+		row := strings.Split(record[0], "\t")
 		for i, rec := range row {
 			if i+1 == field {
 				fmt.Println(rec)
 			}
 		}
+		// fmt.Println(record)
+		// fmt.Println(len(record))
 	}
 	return nil
 }
